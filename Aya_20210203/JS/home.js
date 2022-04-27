36d125601f9bfbbd541bb107ebde1623e5fc6772
Aya <ayaashraf1234321@gmail.com>
2022-04-27 04:46:28 +0200

    home

---
+++ Aya_20210203/JS/home.js
@@ -0,0 +1,71 @@
+// products buttons
+
+let button1 = document.getElementById("button1");
+let button2 = document.getElementById("button2");
+let button3 = document.getElementById("button3");
+let button4 = document.getElementById("button4");
+let button5 = document.getElementById("button5");
+let button6 = document.getElementById("button6");
+let button7 = document.getElementById("button7");
+let button8 = document.getElementById("button8");
+let button9 = document.getElementById("button9");
+let button10 = document.getElementById("button10");
+let button11 = document.getElementById("button11");
+let button12 = document.getElementById("button12");
+
+button1.onclick = function() {
+    window.localStorage.item = 0;
+};
+button2.onclick = function() {
+    window.localStorage.item = 1;
+};
+button3.onclick = function() {
+    window.localStorage.item = 2;
+};
+button4.onclick = function() {
+    window.localStorage.item = 3;
+};
+button5.onclick = function() {
+    window.localStorage.item = 4;
+};
+button6.onclick = function() {
+    window.localStorage.item = 5;
+};
+button7.onclick = function() {
+    window.localStorage.item = 6;
+};
+button8.onclick = function() {
+    window.localStorage.item = 7;
+};
+button9.onclick = function() {
+    window.localStorage.item = 8;
+};
+button10.onclick = function() {
+    window.localStorage.item = 9;
+};
+button11.onclick = function() {
+    window.localStorage.item = 10;
+};
+button12.onclick = function() {
+    window.localStorage.item = 11;
+};
+
+// categories buttons
+
+let laptops = document.getElementById("laptops");
+let tvs = document.getElementById("tvs");
+let phones = document.getElementById("phones");
+let wms = document.getElementById("wms");
+
+laptops.onclick = function (){
+    window.localStorage.item = 12;
+};
+tvs.onclick = function (){
+    window.localStorage.item = 16;
+};
+phones.onclick = function (){
+    window.localStorage.item = 20;
+};
+wms.onclick = function (){
+    window.localStorage.item = 24;
+};
