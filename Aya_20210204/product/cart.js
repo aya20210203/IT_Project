21755c1739f318c382b256cdd172813f4cda77a8
415449415449$$$ <ayaaboud43@gmail.com>
2022-04-30 15:50:49 +0200

    cart&feedback

--- Aya_20210204/product/cart.js
+++ Aya_20210204/product/cart.js
@@ -1,11 +1,13 @@
 function getCartItems(){
-     let index = JSON.parse(localStorage.getItem('usercart')) ;  
+    let index = JSON.parse(localStorage.getItem('usercart')) ;  
     console.log(index);
     for(let i=0;i<index.length;i++)
     {
         let id =index[i];
         let ele =document.getElementById(id);
         ele.style.display = "table-row";
+
+        
     }
     
  }
