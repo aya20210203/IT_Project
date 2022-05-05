d2ce9303ad4ab854abd55c005e56a4fbc8d41f1a
Basmala <hassanbasmala907@gmail.com>
2022-05-06 01:42:26 +0200

    project

--- Basmala_20210223/js pro/pay.js
+++ Basmala_20210223/js pro/pay.js
@@ -3,24 +3,31 @@ fawry.onclick = function () {
     
         messag.style.display = "block";
         hid.style.display="none";
-        phon.style.display="none"    
+        phon.style.display="none";
+        aler_text.style.display="none";   
    
 }
 voda.onclick= function () {
         phon.style.display = "block";
-    
         hid.style.display="none";
-        messag.style.display="none"; 
+        messag.style.display="none";
+        aler_text.style.display="none";
 }
 add.onclick= function () {
     hid.style.display="block";
     messag.style.display="none";
     phon.style.display="none";
+    aler_text.style.display="none";
 
 }
 cash.onclick= function () {
     hid.style.display="none";
     messag.style.display="none";
     phon.style.display="none";
+    aler_text.style.display="none";
+}
+text.onclick= function () {
+    aler_text.style.display="block";
+
 }
 
